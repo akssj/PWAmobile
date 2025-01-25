6a6e13fa4d6be47209b924d87e4b39589f208464
TotalyActiveGitUster <radek.czyzowicz@gmail.com>
2025-01-25 14:13:30 +0100

    removing login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
 import Home from './Components/Home/Home';
 import Account from './Components/Account/Account';
 import CurrencyDetails from './Components/CurrencyDetails/CurrencyDetails';
@@ -8,8 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginSignUp />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/account" element={<Account />} />
         <Route path="/currency/:currencyCode" element={<CurrencyDetails />} />
       </Routes>
